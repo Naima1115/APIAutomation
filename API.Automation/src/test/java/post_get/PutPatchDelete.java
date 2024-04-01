3f490ce7c90c980b8525e2c879b928d7a4d7654b
naima <naimaakter1115@gmail.com>
2024-03-31 21:02:52 -0500

    my api karate

--- API.Automation/src/test/java/post_get/PutPatchDelete.java
+++ API.Automation/src/test/java/post_get/PutPatchDelete.java
@@ -1,13 +1,76 @@
 package post_get;
 
+import static io.restassured.RestAssured.*;
+
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
 
 public class PutPatchDelete {
 	
 	
   @Test
-  public void f() {
+  public void testPut() {
 	  
+	  //Here we will use baseURI and end point
+	  // put means edit something
+	  JSONObject req = new JSONObject();
+	   req.put("name", "Rose");
+	   req.put("Job", "Teacher");
+	   
+	   System.out.println(req.toJSONString());
+       
+	   //now we will test the above put request(update)
+	  
+	   baseURI = "https://reqres.in/api";
+	   
+	  given().
+	  header("Content-Type", "application/json").
+	  contentType(ContentType.JSON).
+	  accept(ContentType.JSON).
+	  body(req.toJSONString()).
+	  when().put("/users/2").  //end point
+	  then().statusCode(200).
+	  log().all();
+ 
+  }
+  
+  
+  @Test
+  public void testPatch() {
+	  
+	  JSONObject req = new JSONObject();
+	   req.put("name", "Rose");
+	   req.put("Job", "Teacher");
+	   
+	   System.out.println(req.toJSONString());
+      
+	   //now we will test the above patch request(update)
+	  
+	   baseURI = "https://reqres.in";
+	   
+	  given().
+	  header("Content-Type", "application/json").
+	  contentType(ContentType.JSON).
+	  accept(ContentType.JSON).
+	  body(req.toJSONString()).
+	  when().patch("/api/users/2").  //end point
+	  then().statusCode(200).
+	  log().all();
 	  
   }
+  
+  @Test
+  public void testDelete() {
+	  
+	  
+	   baseURI = "https://reqres.in";
+	   
+	  given().
+	  when().delete("/api/users/2").  //end point
+	  then().statusCode(204).
+	  log().all();
+  }  
+  
 }
---
+++ API_KARATE/src/main/java/API/API_KARATE/App.java
@@ -0,0 +1,13 @@
+package API.API_KARATE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
